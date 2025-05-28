2fd4413a752d52270d577b4ffd335f80c8fcd3f5
compraxio <luisangelacu10@gmail.com>
2025-05-28 11:00:17 -0500

    se agrego la pagina de proveedores

--- main.py
+++ main.py
@@ -9,7 +9,9 @@
 # - request: para manejar datos del formulario
 # - flash: para mostrar mensajes temporales (notificaciones)
 # - redirect: para redirigir después de enviar el formulario
-from flask import Flask, render_template, url_for, request, flash, redirect
+from flask import Flask, render_template, url_for, request, flash, redirect, send_file
+from flask_sqlalchemy import SQLAlchemy
+import io
 
 # Importa Flask-Mail:
 # - Mail: para configurar el servidor de correos
@@ -22,7 +24,24 @@ from flask_mail import Mail, Message
 
 # Crea la instancia principal de la aplicación Flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///web.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class proveedores(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    identificacion = db.Column(db.String(200), nullable=False)
+    contacto = db.Column(db.String(100), nullable=False)
+    ubicacion = db.Column(db.String(100), nullable=False)
+    condiciones_de_pago = db.Column(db.String(200), nullable=False)
+    ofrece = db.Column(db.String(300), nullable=False)  # Podría ser otro modelo
+    precio = db.Column(db.String(100), nullable=False)  # Para cálculos
+    tiempo_de_entrega = db.Column(db.String(100), nullable=False)
+    imagen = db.Column(db.LargeBinary, nullable=True)  # Para almacenar la imagen en binario
+    
+
+with app.app_context():
+    db.create_all()
 # Clave secreta necesaria para usar "flash" y mantener la sesión segura
 app.secret_key = 'a94a8fe5cc5c2b0e5d8cb6e00392e5ed5557788983e1f915d2cdf697ec811b17'
 
@@ -98,10 +117,80 @@ def optimizacion_de_rutas():
 def gestion_de_vehiculos():
     return render_template('Gestion-de-vehiculos.html')
 
-@app.route('/proveedores')
-def proveedores():
-    return render_template('Proveedores.html')
+@app.route('/imagen_proveedor/<int:proveedor_id>')
+def imagen_proveedor(proveedor_id):
+    proveedor = proveedores.query.get_or_404(proveedor_id)
+    if proveedor.imagen:
+        return send_file(io.BytesIO(proveedor.imagen), mimetype='image/jpeg')
+    else:
+        return redirect(url_for('static', filename='img/proveedor-alternativo.png'))
 
+@app.route('/eliminar_proveedor/<int:proveedor_id>' , methods=['POST'])
+def eliminar_proveedor(proveedor_id):
+    proveedor = proveedores.query.get_or_404(proveedor_id)
+    try:
+        db.session.delete(proveedor)
+        db.session.commit()
+    except Exception as e:
+        print(f"Error al eliminar el proveedor: {e}")
+    return redirect(url_for('proveedor'))
+
+@app.route('/proveedor')
+def proveedor():
+    empresarios = proveedores.query.all()  # Obtiene todos los proveedores de la base de datos
+    return render_template('Proveedores.html' , empresarios=empresarios)
+
+@app.route('/añadir_proveedor', methods=['GET', 'POST'])
+def añadir_proveedor():
+    if request.method == 'POST':
+        imagen_file = request.files['imagen']
+        imagen = imagen_file.read() if imagen_file else None
+        proveedor = request.form['proveedor']
+        Contacto = request.form['Contacto']
+        Ubicacion = request.form['Ubicacion']
+        Condicion = request.form['Condicion']
+        ofrece = request.form['ofrece']
+        Precio = request.form['Precio']
+        tiempo = request.form['tiempo']
+        try:
+            NuevoProveedor = proveedores(
+                identificacion=proveedor,
+                contacto=Contacto,
+                ubicacion=Ubicacion,
+                condiciones_de_pago=Condicion,
+                ofrece=ofrece,
+                precio=Precio,
+                tiempo_de_entrega=tiempo,
+                imagen=imagen
+            )
+            db.session.add(NuevoProveedor)
+            db.session.commit()
+            return redirect("/proveedor")
+        except Exception as e:
+            print(f"Error al guardar: {e}")
+            return redirect(url_for('proveedor'))
+    return render_template('Añadir-proveedor.html')
+
+@app.route('/editar_proveedor/<int:proveedor_id>', methods=['GET', 'POST'])
+def editar_proveedor(proveedor_id):
+    proveedor = proveedores.query.get_or_404(proveedor_id)
+    if request.method == 'POST':
+        imagen_file = request.files['imagen']
+        proveedor.identificacion = request.form['proveedor']
+        proveedor.contacto = request.form['Contacto']
+        proveedor.ubicacion = request.form['Ubicacion']
+        proveedor.condiciones_de_pago = request.form['Condicion']
+        proveedor.ofrece = request.form['ofrece']
+        proveedor.precio = request.form['Precio']
+        proveedor.tiempo_de_entrega = request.form['tiempo']
+        proveedor.imagen = imagen_file.read() if imagen_file else proveedor.imagen
+        try:
+            db.session.commit()
+            return redirect("/proveedor")
+        except Exception as e:
+            print(f"Error al actualizar: {e}")
+            return redirect(url_for('proveedor'))
+    return render_template('Editar-proveedor.html', proveedor=proveedor)
 # =============================================
 # 3. EJECUCIÓN DE LA APLICACIÓN
 # =============================================
